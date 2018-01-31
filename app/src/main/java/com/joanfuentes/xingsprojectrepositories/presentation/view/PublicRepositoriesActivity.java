76648d8b5790f73c4fd2b3dfa0ee1368759cca8d
Joan Fuentes <nescafemix@gmail.com>
2018-01-31 15:16:17 +0100

    created list layout and item list layout

--- app/src/main/java/com/joanfuentes/xingsprojectrepositories/presentation/view/PublicRepositoriesActivity.java
+++ app/src/main/java/com/joanfuentes/xingsprojectrepositories/presentation/view/PublicRepositoriesActivity.java
@@ -1,13 +1,23 @@
 package com.joanfuentes.xingsprojectrepositories.presentation.view;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
 
 import com.joanfuentes.xingsprojectrepositories.Application;
 import com.joanfuentes.xingsprojectrepositories.R;
+import com.joanfuentes.xingsprojectrepositories.domain.model.Repo;
 import com.joanfuentes.xingsprojectrepositories.presentation.view.internal.di.DaggerRuntimeActivityComponent;
 import com.joanfuentes.xingsprojectrepositories.presentation.view.internal.di.RuntimeActivityModule;
 
+import java.util.List;
+
+import butterknife.BindView;
+
 public class PublicRepositoriesActivity extends BaseActivity {
+    @BindView(R.id.item_list) RecyclerView recyclerView;
+    @BindView(R.id.swipe_container) SwipeRefreshLayout swipeRefreshLayout;
+    private List<Repo> repos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +40,22 @@ public class PublicRepositoriesActivity extends BaseActivity {
     }
 
     @Override
-    void onViewReady() {}
+    void onViewReady() {
+        setSwipe2Refresh();
+    }
+
+    private void setSwipe2Refresh() {
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                //TODO: call refresh process
+            }
+        });
+        swipeRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+    }
+
 }
