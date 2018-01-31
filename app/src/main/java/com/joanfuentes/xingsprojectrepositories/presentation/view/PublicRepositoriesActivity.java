d441308ca53ebd3d5b5e954359320c210ff9ddbb
Joan Fuentes <nescafemix@gmail.com>
2018-01-31 15:33:26 +0100

    added ReposAdapter and recyclerview to show list of repositories

--- app/src/main/java/com/joanfuentes/xingsprojectrepositories/presentation/view/PublicRepositoriesActivity.java
+++ app/src/main/java/com/joanfuentes/xingsprojectrepositories/presentation/view/PublicRepositoriesActivity.java
@@ -1,8 +1,11 @@
 package com.joanfuentes.xingsprojectrepositories.presentation.view;
 
 import android.os.Bundle;
+import android.support.v4.widget.ContentLoadingProgressBar;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.joanfuentes.xingsprojectrepositories.Application;
 import com.joanfuentes.xingsprojectrepositories.R;
@@ -10,14 +13,20 @@ import com.joanfuentes.xingsprojectrepositories.domain.model.Repo;
 import com.joanfuentes.xingsprojectrepositories.presentation.view.internal.di.DaggerRuntimeActivityComponent;
 import com.joanfuentes.xingsprojectrepositories.presentation.view.internal.di.RuntimeActivityModule;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.inject.Inject;
 
 import butterknife.BindView;
 
 public class PublicRepositoriesActivity extends BaseActivity {
     @BindView(R.id.item_list) RecyclerView recyclerView;
     @BindView(R.id.swipe_container) SwipeRefreshLayout swipeRefreshLayout;
+    @BindView(R.id.initial_progressbar) ContentLoadingProgressBar contentLoadingProgressBar;
     private List<Repo> repos;
+
+    @Inject ReposAdapter recyclerViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +64,35 @@ public class PublicRepositoriesActivity extends BaseActivity {
                 android.R.color.holo_green_light,
                 android.R.color.holo_orange_light,
                 android.R.color.holo_red_light);
+    }
 
+    public void renderRepos(List<Repo> repos) {
+        showList();
+        if (recyclerView.getAdapter() == null) {
+            this.repos = new ArrayList<>(repos);
+            setupFirstTimeRecyclerView(this.repos);
+        } else {
+            this.repos.clear();
+            this.repos.addAll(repos);
+            updateDataOnRecyclerView();
+        }
+    }
+
+    private void showList() {
+        contentLoadingProgressBar.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.VISIBLE);
+        swipeRefreshLayout.setRefreshing(false);
+    }
+
+    private void setupFirstTimeRecyclerView(List<Repo> repos) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerViewAdapter.setData(repos);
+        recyclerView.setAdapter(recyclerViewAdapter);
+    }
+
+    private void updateDataOnRecyclerView() {
+        recyclerViewAdapter.notifyDataSetChanged();
     }
 
 }
