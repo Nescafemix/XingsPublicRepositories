131f70153018135729c5166fb3f1efa03db05e18
Joan Fuentes <nescafemix@gmail.com>
2018-02-01 19:27:41 +0100

    fixed and improved ReposRepository Unit Tests

--- app/src/test/java/com/joanfuentes/xingsprojectrepositories/data/ReposRepositoryUnitTest.java
+++ app/src/test/java/com/joanfuentes/xingsprojectrepositories/data/ReposRepositoryUnitTest.java
@@ -2,11 +2,13 @@ package com.joanfuentes.xingsprojectrepositories.data;
 
 import com.joanfuentes.xingsprojectrepositories.UnitTest;
 import com.joanfuentes.xingsprojectrepositories.data.datasource.ReposCloudDataSource;
+import com.joanfuentes.xingsprojectrepositories.data.datasource.ReposLocalDataSource;
 import com.joanfuentes.xingsprojectrepositories.domain.model.Repo;
 
 import org.junit.Test;
 import org.mockito.Mock;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Matchers.eq;
@@ -17,20 +19,39 @@ public class ReposRepositoryUnitTest extends UnitTest{
     private ReposRepository reposRepository;
     private int FIRST_PAGE = 1;
     @Mock ReposRepository.Callback reposCallbackMock;
+    @Mock ReposLocalDataSource reposLocalDataSource;
     @Mock ReposCloudDataSource reposCloudDataSource;
-    @Mock List<Repo> reposMock;
+    @Mock Repo repoMock;
+    private List<Repo> emptyRepos;
+    private List<Repo> filledRepos;
+
     @Override
     protected void onSetup() {
-        reposRepository = new ReposRepository(reposCloudDataSource);
+        reposRepository = new ReposRepository(reposLocalDataSource, reposCloudDataSource);
+        emptyRepos = new ArrayList<>();
+        filledRepos = new ArrayList<>();
+        filledRepos.add(repoMock);
     }
 
     @Test
-    public void shouldGetRepos() {
-        doReturn(reposMock).when(reposCloudDataSource).getRepos(FIRST_PAGE);
+    public void shouldGetReposFromLocalDataSource() {
+        doReturn(filledRepos).when(reposLocalDataSource).getRepos(FIRST_PAGE);
 
         reposRepository.getRepos(FIRST_PAGE, reposCallbackMock);
 
+        verify(reposLocalDataSource).getRepos(eq(FIRST_PAGE));
+        verify(reposCallbackMock).onSuccess(eq(filledRepos));
+    }
+
+    @Test
+    public void shouldGetReposFromCloudDataSource() {
+        doReturn(emptyRepos).when(reposLocalDataSource).getRepos(FIRST_PAGE);
+        doReturn(filledRepos).when(reposCloudDataSource).getRepos(FIRST_PAGE);
+
+        reposRepository.getRepos(FIRST_PAGE, reposCallbackMock);
+
+        verify(reposLocalDataSource).getRepos(eq(FIRST_PAGE));
         verify(reposCloudDataSource).getRepos(eq(FIRST_PAGE));
-        verify(reposCallbackMock).onSuccess(eq(reposMock));
+        verify(reposCallbackMock).onSuccess(eq(filledRepos));
     }
 }
